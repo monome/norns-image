440345da9406c1318182f3348e1996d5a7b6f284
rick venn <rv@lap.chop>
2018-01-28 14:51:01 +0000

    auto-generate trivial wpa_supplicant.conf

--- scripts/wifi-scan.sh
+++ scripts/wifi-scan.sh
@@ -4,7 +4,10 @@ WIFI_INTERFACE=$(ip addr|grep 2: | awk '{print $2}'|sed -e s/:$//)
 WPA_PS=$(ps aux | grep wpa_supplicant |grep -v grep | awk '{print $2}')
 if [ -z $WPA_PS ]; then
     echo "starting wpa_supplicant..."
-    sudo wpa_supplicant -B -i$WIFI_INTERFACE -c$HOME/wpa_supplicant.conf
+    WPA_FILE=$HOME/wpa_supplicant.conf
+    echo ctrl_interface=/run/wpa_supplicant > $WPA_FILE
+    echo update_config=1 >> $WPA_FILE
+    sudo wpa_supplicant -B -i$WIFI_INTERFACE -c$WPA_FILE
 fi
 
 sudo wpa_cli scan > /dev/null;
