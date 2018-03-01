d0d29d79ad642281c62527b9a6bee011613575d8
Brian Crabtree <tehn@monome.org>
2018-03-01 10:37:47 -0500

    update init script

--- scripts/init-norns.sh
+++ scripts/init-norns.sh
@@ -11,19 +11,21 @@ echo "37" > /sys/class/gpio/export
 echo "39" > /sys/class/gpio/export
 
 
-# turn analog gain stages to unity
-
+# turn analog gain stages to unity 
+# below is for output ch 1/2. note this gain stage isn't present on production models.
+# only applicable to round 1 prototypes. this will throw an error on others.
 sudo i2cset -y 1 0x28 0
 sudo i2cset -y 1 0x28 64
-sudo i2cset -y 1 0x29 0
-sudo i2cset -y 1 0x29 64
 
-# unmute soundcard output
+# input gain stage. the default is mute so we skip this. we let matron turn it on. 
+# but the values below are sane defaults otherwise (unity gain)
+#sudo i2cset -y 1 0x29 16
+#sudo i2cset -y 1 0x29 80
 
+# unmute soundcard output 
 amixer set Master on 
 
-# enable headphone driver
-
+# enable headphone driver 
 sudo i2cset -y 1 0x60 1 192    # enable HP outputs
 sudo i2cset -y 1 0x60 2 32     # unmute, set vol to -10db 
 #sudo i2cset -y 1 0x60 2 52     # unmute, set vol to 0.1db
@@ -56,8 +58,7 @@ echo "in" > /sys/class/gpio/gpio39/direction
 echo "both" > /sys/class/gpio/gpio39/edge
 
 # start norns
-su pi -c "cd /home/pi/norns; ./start.sh;"
+#su pi -c "cd /home/pi/norns; ./start.sh;"
 
 # clean up stale wifi status from shutdown
-echo stopped > $HOME/status.wifi
-
+echo stopped > $HOME/status.wifi 
