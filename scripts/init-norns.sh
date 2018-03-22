626ae9d1c526c16b13706991286b9e5648a6c029
brian crabtree <tehn@monome.org>
2018-03-22 18:46:49 -0400

    faster encoders, simpler startup script

--- scripts/init-norns.sh
+++ scripts/init-norns.sh
@@ -1,15 +1,5 @@
 #init gpio
 
-echo "28" > /sys/class/gpio/export
-echo "29" > /sys/class/gpio/export
-echo "31" > /sys/class/gpio/export
-echo "32" > /sys/class/gpio/export
-echo "33" > /sys/class/gpio/export
-echo "35" > /sys/class/gpio/export
-echo "36" > /sys/class/gpio/export
-echo "37" > /sys/class/gpio/export
-echo "39" > /sys/class/gpio/export
-
 # turn analog gain stages to unity
 # note this gain stage is only applicable to round 1 prototypes
 # this will throw an error on others.
@@ -26,37 +16,10 @@ sudo i2cset -y 1 0x28 0x40
 # unmute soundcard output
 amixer set Master 100% on
 
-# enable headphone driver
-sudo i2cset -y 1 0x60 1 192    # enable HP outputs
-sudo i2cset -y 1 0x60 2 32     # unmute, set vol to -10db
+# enable headphone driver (let matron handle this)
+#sudo i2cset -y 1 0x60 1 192    # enable HP outputs
+#sudo i2cset -y 1 0x60 2 32     # unmute, set vol to -10db
 #sudo i2cset -y 1 0x60 2 52     # unmute, set vol to 0.1db
-
-echo "in" > /sys/class/gpio/gpio28/direction
-echo "both" > /sys/class/gpio/gpio28/edge
-
-echo "in" > /sys/class/gpio/gpio29/direction
-echo "both" > /sys/class/gpio/gpio29/edge
-
-echo "in" > /sys/class/gpio/gpio31/direction
-echo "both" > /sys/class/gpio/gpio31/edge
-
-echo "in" > /sys/class/gpio/gpio32/direction
-echo "both" > /sys/class/gpio/gpio32/edge
-
-echo "in" > /sys/class/gpio/gpio33/direction
-echo "both" > /sys/class/gpio/gpio33/edge
-
-echo "in" > /sys/class/gpio/gpio35/direction
-echo "both" > /sys/class/gpio/gpio35/edge
-
-echo "in" > /sys/class/gpio/gpio36/direction
-echo "both" > /sys/class/gpio/gpio36/edge
-
-echo "in" > /sys/class/gpio/gpio37/direction
-echo "both" > /sys/class/gpio/gpio37/edge
-
-echo "in" > /sys/class/gpio/gpio39/direction
-echo "both" > /sys/class/gpio/gpio39/edge
 
 # start norns
 su pi -c "cd /home/pi/norns; ./start.sh;"
