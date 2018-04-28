6b50989517dd88d47924b395560c647d7d10cb52
Artem Popov <artfwo@gmail.com>
2018-04-28 14:09:27 +0700

    remove unnecessary realtime group stuff

--- setup.sh
+++ setup.sh
@@ -3,11 +3,6 @@ sudo cp config/cmdline.txt /boot/
 sudo cp config/jackdrc ~/.jackdrc
 sudo cp config/rc.local /etc/
 
-#realtime group
-sudo groupadd realtime
-sudo usermod -a -G realtime pi
-sudo cp config/99-realtime.conf /etc/security/limits.d/99-realtime.conf
-
 #wifi hotspot
 sudo cp config/dnsmasq.conf /etc/dnsmasq.conf
 sudo cp config/hostapd /etc/default/hostapd
